bf117e7f9017584f11b05ee59ccad7ff7275ede8
Clayton <cscavone96@gmail.com>
2017-10-05 19:12:35 -0400

    Finished

--- src/server.js
+++ src/server.js
@@ -18,11 +18,65 @@ console.log(`Listening on 127.0.0.1: ${port}`);
 
 const io = socketio(app);
 
+let block = {};
+
+const users = [];
+
+const points = {};
+
+const emitBlock = () => {
+  let newBlock = {};
+  const xPos = Math.floor((Math.random() * (480 - 50)) + 50);
+  const yPos = Math.floor((Math.random() * (480 - 50)) + 50);
+  const xSize = Math.floor((Math.random() * (200 - 50)) + 50);
+  const ySize = Math.floor((Math.random() * (200 - 50)) + 50);
+  if (Object.keys(block).length === 0 && block.constructor === Object) {
+    newBlock = { x: xPos, y: yPos, width: xSize, height: ySize };
+    block = newBlock;
+  } else {
+    newBlock = block;
+  }
+  io.sockets.in('room1').emit('createBlock', newBlock);
+};
+
+
+const emitPlayers = () => {
+  let data = '';
+  users.forEach((element) => {
+    data = `${data}<li>${element.name} - ${points[element.name]}</li>`;
+  });
+  io.sockets.in('room1').emit('clientUpdate', data);
+};
+
+
+const onJoined = (sock) => {
+  const socket = sock;
+  socket.on('join', (data) => {
+    socket.name = data.name;
+    users.push(socket);
+    socket.join('room1');
+    points[socket.name] = 0;
+    console.log(`${data.name} joined`);
+    emitBlock(socket);
+    emitPlayers(socket);
+  });
+};
+
 io.sockets.on('connection', (socket) => {
   console.log('started');
-  socket.on('draw', (data) => {
-    console.log("on draw");
-    io.emit('update', data);
+  onJoined(socket);
+  socket.on('blockClicked', (name) => {
+    points[name]++;
+    block = {};
+    emitBlock(socket);
+    emitPlayers(socket);
+  });
+
+  socket.on('disconnect', () => {
+    const userInd = users.indexOf(socket.name);
+    users.splice(userInd, 1);
+    emitPlayers(socket);
+    socket.leave('room1');
   });
 });
 
